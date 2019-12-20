4718cd778ecff123f10d50c4fb1c8b3cd12bf785
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2019-12-20 05:09:41 -0500

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -17,34 +17,53 @@ class createTree:
         if self.depth >= 0:
             branchCount = random.randint(1,10)
             for i in range(1,branchCount):
-                self.children.append(createTree(self.depth - 1,-self.player))
+                self.children.append(createTree(self.depth-1,-self.player))
 
 tree = createTree(4,1)
 
 linesList = []
 
-def DiffiMax(node,depth,player,drawThresh,winThresh,yourNum,candidFactor):
+
+# drawThresh is always positive
+# winThresh is always positive
+# yourNum is the color of "player": 1 for white, -1 for black
+
+def DiffiMax(node,depth,player,drawThresh,winThresh,yourNum):
+    if (abs(node.value) >= drawThresh) & (node.player == yourNum): #TODO
+        
+        
     if depth == 0:
-        pass
+            return node, node.value
     
     if len(node.children) == 1: # If node only has one child
-        MiniMax()
-    elif player == -yourNum: # Only looks for traps for chosen color and uses MiniMax for your moves
+        DiffiMax(node.children[0],depth-1,drawThresh,winThresh,yourNum)
+    
+    elif node.player == yourNum: # Only looks for traps for opponent and uses MiniMax for player's moves
         childVals = []
+        
         for i in range(len(node.children)):
             childVals.append((i,node.children[i].value)) # Makes list of children values
         
-        if player == -1: # Sorts and calculates according to player
+        if node.player == -1: # Sorts and calculates according to player's color, which is white here
             sortList = sorted(childVals, reverse=True, key=lambda a:a[1]) 
-            if (len(node.children)*(sortList[0][1] - sortList[1][1])/candidFactor >= winThresh) & (sortList[1][1] >= winThresh) & (node.children.check != 1) & (node.children.capture != 1):
-                MiniMax(node.children[sortList[1][0]])
+            
+            if (sortList[0][1] - sortList[1][1] >= winThresh) & (sortList[1][1] >= winThresh) & (node.check != 1) & (node.capture != 1)\
+                & (node.children[sortList[0][0]].capture != 1) & (node.children[sortList[1][0]].capture != 1):  
+                
+                DiffiMax(node.children[sortList[1][0]])
+            
             else:
                 MiniMax()
-        else:
+        else: # If calculating for player with black pieces
             sortList = sorted(childVals, key=lambda a:a[1])
-            if (len(node.children)*(sortList[0][1] - sortList[1][1])/candidFactor <= winThresh) & (sortList[1][1] <= winThresh) & (node.children.check != 1) & (node.children.capture != 1):
-                MiniMax(node.children[sortList[1][0]])
+            
+            if (sortList[0][1] - sortList[1][1] <= winThresh) & (sortList[1][1] <= winThresh) & (node.check != 1) & (node.capture != 1)\
+                & (node.children[sortList[0][0]].capture != 1) & (node.children[sortList[1][0]].capture != 1):  
+                
+                DiffiMax(node.children[sortList[1][0]])
+            
             else:
-                MiniMax()
-    else:
-        MiniMax()
+                DiffiMax()
+    
+    else: # When player's move
+        DiffiMax()
