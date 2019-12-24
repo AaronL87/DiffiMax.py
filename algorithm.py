1091e2d145793fd44126ae6b70886e6d894bebd1
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2019-12-24 17:03:34 -0500

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -20,32 +20,29 @@ class createTree:
                 self.children.append(createTree(self.depth-1,player=-self.player))
         elif self.depth == 0:
             self.value = random.gauss(.1,1)
-
-linesList = []
-
-# drawThresh is always positive
-# winThresh is always positive
-# yourNum is the color of "player": 1 for white, -1 for black
-
-# TODO First check if Minimax leads to player beating opponent
-# TODO If optimal line has player lose, can still choose traps to increase practical chances
-
-def DiffiMax(node,drawThresh,winThresh,yourNum,linesList):
+            
+def DiffiMax(node,drawThresh,winThresh,yourNum):
     # TODO Look for last DiffiMax trap continuation in lineList and delete it. Run MiniMax there instead.
     # TODO If there are no DiffiMax traps in lineList, then player is lost because only MiniMax has been used
     print(1,node.depth)
     if node.depth != 1: # Cycles through all nodes
         for child in node.children:
-            node.value, linesList = DiffiMax(child,drawThresh,winThresh,yourNum,linesList)
-            print(node.value)
-        return node.value, linesList
+            child.value = DiffiMax(child,drawThresh,winThresh,yourNum)
+            print(child.value)
+            
+        node.value = DiffiSort(node,drawThresh,winThresh,yourNum)
+        return node.value
+    
+    DiffiSort(node,drawThresh,winThresh,yourNum)
+    
+    
+def DiffiSort(node,drawThresh,winThresh,yourNum):
     print(2,node.depth)
     node.children.sort(reverse=True,key=lambda a:a.value) # Sorts children for future selections
     print(3)
     if node.player == -yourNum: # If it is your opponent's choice
         if len(node.children) == 1: # If node only has one child. Checked because next condition requires two children.
-            linesList = [(node.children[0],node.children[0].value)]+linesList
-            return node.children[0].value,linesList
+            return node.children[0].value
         elif node.player == -1: # Opponent has black pieces
             # Checks for difficult positions for opponent while accounting for forcing moves:
             if (node.children[-1].value - node.children[-2].value <= -winThresh) & (node.children[-2].value >= winThresh)\
@@ -53,34 +50,26 @@ def DiffiMax(node,drawThresh,winThresh,yourNum,linesList):
                                                                                 & (node.check != 1) & (node.capture != 1)\
                                                                                 & (node.children[-1].capture != 1)\
                                                                                 & (node.children[-2].capture != 1):     
-                linesList = [(node.children[-2],node.children[-2].value,node.children[-1].value)]+linesList
-                return node.children[-2].value, linesList
+                return node.children[-2].value
             else:
-                linesList = [(node.children[-1],node.children[-1].value)]+linesList
-                return node.children[-1].value, linesList
+                return node.children[-1].value
         elif node.player == 1: # Opponent has white pieces
             if (node.children[0].value - node.children[1].value >= winThresh) & (node.children[1].value <= -winThresh)\
                                                                                 & (node.children[0].value <= drawThresh)\
                                                                                 & (node.check != 1) & (node.capture != 1)\
                                                                                 & (node.children[0].capture != 1)\
                                                                                 & (node.children[1].capture != 1):  
-                linesList = [(node.children[1],node.children[1].value,node.children[0].value)]+linesList
-                return node.children[1].value, linesList 
+                return node.children[1].value
             else:
-                linesList = [(node.children[0],node.children[0].value)]+linesList
-                return node.children[0].value, linesList
+                return node.children[0].value
     elif node.player == yourNum: # If it is your choice
         if node.player == 1: # You have the white pieces
-            linesList = [(node.children[0],node.children[0].value)]+linesList
-            return node.children[0].value, linesList
+            return node.children[0].value
         
         elif node.player == -1: # You have the black pieces
-            linesList = [(node.children[-1],node.children[-1].value)]+linesList
-            return node.children[-1].value, linesList
+            return node.children[-1].value
 
 tree = createTree(4)
 # If depth == even, then algorithm begins with opponent choice (because choice starts with depth 1)
 
-finalEval, linesList = DiffiMax(tree,1,2.5,1,linesList)
-
-print(finalEval)
+DiffiMax(tree,1,2.5,1)
